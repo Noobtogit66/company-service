388fd72cacde02ff1ad7b21da94f9821fdc0cd30
Noobtogit66 <janasagnik66@gmail.com>
2025-12-10 21:41:37 +0530

    blank commit

---
+++ src/test/java/com/sagnik/companyserver/CompanyserverApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sagnik.companyserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CompanyserverApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
